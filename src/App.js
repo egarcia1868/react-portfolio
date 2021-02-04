44474b122922faa92a70f28af3a32ba309334c5c
Eric Garcia <egarcia1868@gmail.com>
2021-02-04 10:42:53 -0600

    adding "Under construction" alert

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "../src/components/Main";
 import Footer from "../src/components/Footer";
 
 function App() {
+  alert("site under construction as of 3/4/21");
   return (
     <div>
       <Header />
